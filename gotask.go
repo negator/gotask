89141e20e53da78190e4412f83ef738dbacb47d8
negator <naveen.gattu@gmail.com>
2016-05-03 12:32:33 -0700

    Use channels instead of wait groups

--- gotask.go
+++ gotask.go
@@ -1,41 +1,54 @@
 package gotask
 
 import (
+	"reflect"
 	"sync"
 )
 
+type GoTask struct {
+	/*Make sure done is called only once*/
+	once sync.Once
+	done chan bool
+}
+
+/*Block until task is finished*/
+func (t *GoTask) Await() {
+	t.once.Do(func() {
+		<-t.done
+	})
+}
+
+func goTask(f func()) *GoTask {
+	r := GoTask{done: make(chan bool, 1)}
+	go func() {
+		f()
+		r.done <- true
+	}()
+	return &r
+}
+
 type GoTaskGroup struct {
+	once  sync.Once
 	tasks []*GoTask
 }
 
-func (g *GoTaskGroup) Append(t *GoTask) {
-	g.tasks = append(g.tasks, t)
+/*Append returns a new GoTaskGroup with the set of tasks or function appended*/
+func (g *GoTaskGroup) Append(t ...*GoTask) GoTaskGroup {
+	tasks := append(g.tasks, t...)
+	return GoTaskGroup{tasks: tasks}
 }
-func (g *GoTaskGroup) AppendF(f func()) {
-	g.Append(task(f))
+func (g *GoTaskGroup) AppendF(f func()) GoTaskGroup {
+	return g.Append(goTask(f))
 }
 
 func (g *GoTaskGroup) AwaitAll() {
-	for _, f := range g.tasks {
-		f.Await()
-	}
-}
-
-type GoTask struct {
-	wg *sync.WaitGroup
-}
-
-func (t *GoTask) Await() {
-	t.wg.Wait()
-}
-
-func task(f func()) *GoTask {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	r := GoTask{wg: &wg}
-	go func() {
-		f()
-		wg.Done()
-	}()
-	return &r
+	g.once.Do(func() {
+		cases := make([]reflect.SelectCase, len(g.tasks))
+		for i, task := range g.tasks {
+			cases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(task.done)}
+		}
+		for r := len(cases); r > 0; r-- {
+			reflect.Select(cases)
+		}
+	})
 }
--- gotask_test.go
+++ gotask_test.go
@@ -11,16 +11,39 @@ func TestGoTaskGroup(b *testing.T) {
 	a := []int{1, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9, 9, 10, 11, 12}
 
 	batchSize := 3
-	tg := GoTaskGroup{}
+	var tg GoTaskGroup
 	c := make(chan []int, 1000)
+	defer close(c)
 	for i := 0; i < len(a); i += batchSize {
 		j := i
-		tg.AppendF(func() {
+		tg = tg.AppendF(func() {
 			s := math.Min(float64(len(a)), float64(j+batchSize))
 			b := a[j:int(s)]
 			c <- b
 			fmt.Println(b)
 		})
 	}
+	tg.AwaitAll()
+}
+
+func TestGoTask(b *testing.T) {
+	c := make(chan int, 1)
+
+	producer := goTask(func() {
+		/*Potentially long running IO like a streaming database call.*/
+		for i := 0; i < 100; i++ {
+			var j = i
+			c <- j
+		}
+		close(c)
+	})
+
+	consumer := goTask(func() {
+		for i := range c {
+			fmt.Printf("Consumed %v\n", i)
+		}
+	})
+	var tg GoTaskGroup
+	tg = tg.Append(producer, consumer)
 	tg.AwaitAll()
 }
